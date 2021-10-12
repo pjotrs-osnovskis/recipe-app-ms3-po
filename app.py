0e35e9da275a2a503670dbb07332af7a8b2f85f7
Pjotrs OSnovskis <pjotrs.osnovskis@gmail.com>
2021-10-12 22:08:03 +0100

    Add static folder with relevant css and js folders and files in them, add templates folder with base.html. Add bootstrap navigation and basic styling for project needs.

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 import os
-from flask import Flask
+from flask import (
+    Flask, flash, render_template, 
+    redirect, request, session, url_for)
 if os.path.exists("env.py"):
     import env
 
@@ -7,7 +9,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello World!"
+    return render_template("base.html")
 
 
 if __name__ == "__main__":
