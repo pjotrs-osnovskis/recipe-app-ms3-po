4f3235fd8284fb74475b14d49602239920e12879
Pjotrs OSnovskis <pjotrs.osnovskis@gmail.com>
2021-10-14 19:15:28 +0100

    Connected to MongoDB and tested connection by displaying all categories in home.html

--- app.py
+++ app.py
@@ -2,14 +2,24 @@ import os
 from flask import (
     Flask, flash, render_template, 
     redirect, request, session, url_for)
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
 if os.path.exists("env.py"):
     import env
 
 app = Flask(__name__)
 
+app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
+app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
+app.secret_key = os.environ.get("SECRET_KEY")
+
+mongo = PyMongo(app)
+
+
 @app.route("/")
-def hello():
-    return render_template("base.html")
+def home_page():
+    categories = mongo.db.categories.find()
+    return render_template("home.html", categories=categories)
 
 
 if __name__ == "__main__":
