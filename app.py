8fae679c768a053c24fe8cb2ccc2e60ad5e580c5
Pjotrs OSnovskis <pjotrs.osnovskis@gmail.com>
2021-10-15 20:49:29 +0100

    Added CRUD ingredients as Admin section for now, to add ingredients to database for app users to choose from

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import os
 from flask import (
-    Flask, flash, render_template, 
+    Flask, render_template, 
     redirect, request, session, url_for)
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
@@ -16,10 +16,47 @@ app.secret_key = os.environ.get("SECRET_KEY")
 mongo = PyMongo(app)
 
 
+# Home Page Route
 @app.route("/")
 def home_page():
     categories = mongo.db.categories.find()
     return render_template("home.html", categories=categories)
+
+
+# Admin - Show list of ingredients and add ingredient
+@app.route("/add_ingredient", methods=("GET", "POST"))
+def add_ingredient():
+    ingredients_list = list(mongo.db.ingredients.find().sort("ingredient_name", 1))
+
+    if request.method == "POST":
+        ingredient = {
+            "ingredient_name": request.form.get("ingredient_name")
+            }
+        mongo.db.ingredients.insert_one(ingredient)
+        return redirect(url_for("add_ingredient"))
+
+    return render_template("admin.html", ingredients=ingredients_list)
+
+
+# Admin - Edit Ingredient
+@app.route("/edit_ingredient/<ingredient_id>", methods=["GET", "POST"])
+def edit_ingredient(ingredient_id):
+    if request.method == "POST":
+        submit = {
+            "ingredient_name": request.form.get("ingredient_name")
+        }
+        mongo.db.ingredients.update({"_id":ObjectId(ingredient_id)}, submit)
+        return redirect(url_for("add_ingredient"))
+
+    ingredient = mongo.db.ingredients.find_one({"_id": ObjectId(ingredient_id)})
+    return render_template("edit_ingredient.html", ingredient=ingredient)
+
+
+# Admin - Delete Ingredient
+@app.route("/delete_ingredient/<ingredient_id>")
+def delete_ingredient(ingredient_id):
+    mongo.db.ingredients.remove({"_id": ObjectId(ingredient_id)})
+    return redirect(url_for("add_ingredient"))
 
 
 if __name__ == "__main__":
