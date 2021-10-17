660861d61ef1f9aa10e7d41076fa126461fd931e
Pjotrs OSnovskis <pjotrs.osnovskis@gmail.com>
2021-10-17 09:54:06 +0100

    Add exclamation mark to confirm password functionality as well as style. updated confirm_password functions - removed unnecessary code

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,14 +1,27 @@
-/*
-    Checking registration form if password and confirm_password match 
-    as well as if not empty then remove disabled attribute from registration button.
-    Code snippet found here:
-    https://newbedev.com/how-to-check-confirm-password-field-in-form-without-reloading-page
-    Then optimized to project needs.
-*/
 $('#reg-password, #confirm_password').on('keyup', function () {
-    if ($('#reg-password').val() != $('#confirm_password').val() || document.forms["regForm"]["password"].value == "" && document.forms["regForm"]["confirm_password"].value == "")  {
-        document.getElementById("regBtn").setAttribute("disabled", "");
-    }else if ($('#reg-password').val() != $('#confirm_password').val() || document.getElementById("regBtn").value == "")
-        document.getElementById("regBtn").removeAttribute("disabled");
+    /*
+        Checking registration form if password and confirm_password match 
+        as well as if not empty then remove disabled attribute from registration button.
+        Code snippet found here:
+        https://newbedev.com/how-to-check-confirm-password-field-in-form-without-reloading-page
+        Then optimized to project needs.
+    */
 
+    if ($('#reg-password').val() != $('#confirm_password').val()) {
+        $("#regBtn").attr("disabled", "");
+
+    } else if ($('#reg-password').val() == $('#confirm_password').val() || $("#regBtn").val() != "") 
+        $("#regBtn").removeAttr("disabled");
+
+});
+
+$('#reg-password, #confirm_password').on('keyup', function () {
+    /*
+        Same as function above, but adds exclamation mark to confirm password
+    */
+    if ($('#reg-password').val() != $('#confirm_password').val())  {
+        $("#exclIcon").css("visibility", "visible")
+
+    } else if ($('#reg-password').val() == $('#confirm_password').val() || $("#regBtn").val() != "")
+        $("#exclIcon").css("visibility", "hidden")
 });
