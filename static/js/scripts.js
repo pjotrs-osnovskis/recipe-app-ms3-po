b8db0859cbe045b58980b1de389dae485ccdfba0
Pjotrs OSnovskis <pjotrs.osnovskis@gmail.com>
2021-10-19 20:26:03 +0100

    Updated ingredients search function and styling

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -14,22 +14,55 @@ $('#reg-password, #confirm_password').on('keyup', function () {
 });
 
 
+// function search() {
+//     // Declare variables
+//     var input, filter, ul, li, a, i, txtValue;
+//     input = document.getElementById('myInput');
+//     filter = input.value.toUpperCase();
+//     ul = document.getElementById("myUL");
+//     li = ul.getElementsByTagName('li');
+  
+//     // Loop through all list items, and hide those who don't match the search query
+//     for (i = 0; i < li.length; i++) {
+//       a = li[i].getElementsByTagName("a")[0];
+//       txtValue = a.textContent || a.innerText;
+//       if (txtValue.toUpperCase().indexOf(filter) > -1) {
+//         li[i].style.display = "";
+//       } else {
+//         li[i].style.display = "none";
+//       }
+//     }
+//   }
+
 function search() {
     // Declare variables
-    var input, filter, ul, li, a, i, txtValue;
-    input = document.getElementById('myInput');
-    filter = input.value.toUpperCase();
-    ul = document.getElementById("myUL");
-    li = ul.getElementsByTagName('li');
-  
-    // Loop through all list items, and hide those who don't match the search query
-    for (i = 0; i < li.length; i++) {
-      a = li[i].getElementsByTagName("a")[0];
-      txtValue = a.textContent || a.innerText;
-      if (txtValue.toUpperCase().indexOf(filter) > -1) {
-        li[i].style.display = "";
-      } else {
-        li[i].style.display = "none";
-      }
-    }
-  }
+    var input, filter, ul, li, a, i;
+    input = document.getElementById('searchInput');
+        if (input.value==''){
+            document.getElementById("myUL").style.display='none';
+            }
+        else{
+            filter = input.value.toUpperCase();
+            ul = document.getElementById("myUL");
+            li = ul.getElementsByTagName('li');
+
+            if(input.value.length == 0){
+                ul.style.display = "none";
+                return;
+            }else{
+                ul.style.display = "block";
+                ul.style.height = "10rem";
+                ul.style.width = "15rem";
+                ul.style.overflow = "auto";
+            }
+            // Loop through all list items, and hide those who don't match the search query
+            for (i = 0; i < li.length; i++) {
+                a = li[i].getElementsByTagName("a")[0];
+                if (a.innerHTML.toUpperCase().indexOf(filter) > -1) {
+                    li[i].style.display = "block";
+                } else {
+                    li[i].style.display = "none";
+                }
+            }
+        }
+}
